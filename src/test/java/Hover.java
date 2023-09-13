12d60a7479520a456f36439f11a5d110e11d49ab
neverxrist <qdonat@Ya.ru>
2023-09-14 01:39:43 +0300

    bit improvement

--- src/test/java/Hover.java
+++ src/test/java/Hover.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Hover {
@@ -16,10 +17,13 @@ public class Hover {
     @Test
     void hover() {
         open("");
-        $("header").$$("ul li").findBy(text("Solutions")).hover()
+        /*$("header").$$("ul li").findBy(text("Solutions")).hover()
                 .$$("[class='HeaderMenu-dropdown dropdown-menu rounded m-0 p-0 py-2 py-lg-4 " +
                         "position-relative position-lg-absolute left-0 left-lg-n3 px-lg-4'] li")
                 .findBy(text("Enterprise")).click();
+         */
+        $("header").$(byTagAndText("button", "Solutions")).hover();
+        $("header").$(byTagAndText("a", "Enterprise")).click();
         $("main.font-mktg").shouldHave(text("Build like the best"));
 
 
