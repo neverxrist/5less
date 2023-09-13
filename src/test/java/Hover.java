ba5dffd8d64a0b956df1bb7d5fbfaf7edf2e5865
neverxrist <qdonat@Ya.ru>
2023-09-14 01:01:30 +0300

    1 of 2 done

---
+++ src/test/java/Hover.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Hover {
+    @BeforeAll
+    static void before() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "2560x1440";
+        Configuration.holdBrowserOpen = false;
+    }
+    @Test
+    void hover() {
+        open("");
+        $("header").$$("ul li").findBy(text("Solutions")).hover()
+                .$$("[class='HeaderMenu-dropdown dropdown-menu rounded m-0 p-0 py-2 py-lg-4 " +
+                        "position-relative position-lg-absolute left-0 left-lg-n3 px-lg-4'] li")
+                .findBy(text("Enterprise")).click();
+        $("main.font-mktg").shouldHave(text("Build like the best"));
+
+
+
+        sleep(5000);
+    }
+}
